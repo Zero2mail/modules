a892246b632009a3b9a134924ba0162ac95b6dce
Zero2mail <71940406+Zero2mail@users.noreply.github.com>
2020-09-26 22:41:12 +0300

    Update mf.py

--- mf.py
+++ mf.py
@@ -1,8 +1,12 @@
+from uniborg.util import admin_cmd
+
+@borg.on(admin_cmd("mf ?(.*?)"))
+async def _(event):
 await event.respond("@LaciaMemeFrame")
-event.respond("@LaciaMemeFrame")
-event.respond("@LaciaMemeFrame")
-event.respond("@LaciaMemeFrame")
-event.respond("@LaciaMemeFrame")
+await event.respond("@LaciaMemeFrame")
+await event.respond("@LaciaMemeFrame")
+await event.respond("@LaciaMemeFrame")
+await event.respond("@LaciaMemeFrame")
 event.respond("@LaciaMemeFrame")
 event.respond("@LaciaMemeFrame")
 event.respond("@LaciaMemeFrame")
