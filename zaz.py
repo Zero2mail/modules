17692168fc1522baee0a7004253b3f9e7c1a2b03
Zero2mail <71940406+Zero2mail@users.noreply.github.com>
2020-09-26 20:57:38 +0300

    Add files via upload

---
+++ zaz.py
@@ -0,0 +1,13 @@
+import time
+from uniborg.util import admin_cmd
+
+@borg.on(admin_cmd("zaz ?(.*?)"))
+async def _(event):
+	await event.edit("ooh") 
+	time.sleep(1)
+	await event.edit("lol") 
+	time.sleep(1) 
+	await event.edit("fuck")
+	time.sleep(1) 
+	await event.delete() 
+	event.respond("made by @hackernel1337")
