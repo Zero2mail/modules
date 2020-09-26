069c12643f275e9574663425fdf0f78e1b80f97c
Zero2mail <71940406+Zero2mail@users.noreply.github.com>
2020-09-26 22:30:38 +0300

    Update Mem.py
    A

--- Mem.py
+++ Mem.py
@@ -1,10 +1,10 @@
 import time
 from uniborg.util import admin_cmd
 
-@borg.on(admin_cmd("zaz ?(.*?)"))
+@borg.on(admin_cmd("mem ?(.*?)"))
 async def _(event):
-await event.edit("Скинь")
-time.sleep(2)
-await event.edit("Мемчики")
-time.sleep(2)
-event.delete()
+  await event.edit("Скинь")
+  time.sleep(1)
+  await event.edit("Мемчики")
+  time.sleep(1)
+  event.delete()
