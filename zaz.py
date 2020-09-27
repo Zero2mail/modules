9f96799f1a34f142023262c1929d030d178d91cb
Zero2mail <71940406+Zero2mail@users.noreply.github.com>
2020-09-27 23:09:51 +0300

    Update zaz.py
    A

--- zaz.py
+++ zaz.py
@@ -21,12 +21,3 @@ async def _(event):
   await event.edit("Мемчики")
   time.sleep(1)
   await event.delete()
-
-@borg.on(admin_cmd("gid ?(.*?)"))
-async def _(event):
-	event.edit(f"ID Чата:  {event.chat_id}")
-
-@borg.on(admin_cmd("uid ?(.*?)"))
-async def _(event):
-	infa = str(event.sender_id)
-	event.edit('ID Юзера: ' + infa)
