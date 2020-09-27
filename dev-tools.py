b3704201020c895bb0a29e12cea0f397e5743892
Zero2mail <71940406+Zero2mail@users.noreply.github.com>
2020-09-27 22:58:14 +0300

    Update dev-tools.py
    В

--- dev-tools.py
+++ dev-tools.py
@@ -1,1 +1,11 @@
-event.edit(f"ID Чата:  {event.chat_id}")
+
+
+
+def register(cb):
+	cb(AudioEditorMod())
+class AudioEditorMod(loader.Module):
+	"""AudioEditor"""
+	strings = {'name': 'AudioEditor'}
+
+
+event.edit(f"ID Чата: {event.chat_id}")
