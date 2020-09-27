f2d2009fded9b4d0c7f4b0561e97e929e07540bf
Zero2mail <71940406+Zero2mail@users.noreply.github.com>
2020-09-27 18:33:46 +0300

    Update spam.py
    .

--- spam.py
+++ spam.py
@@ -1,17 +1,17 @@
 from .. import loader, utils
 import asyncio
-
 def register(cb):
-    cb(pidorMod())
-class pidorMod(loader.Module):
-    """pidor"""
-    strings = {'name': 'SpamPidor'}
+    cb(spamMod())
+class spamMod(loader.Module):
+    """spam"""
+    strings = {'name': 'Spam'}
 
-    async def pidorcmd(self, message):
-        """.pidor <кол-во сообщений для спама>           
+    async def spamcmd(self, message):
+        """.spam <кол-во сообщений для спама> <текст>           
         """
         count = int(utils.get_args_raw(message))
+        textt = str(utils.get_args_raw(message))
         
         for i in range(count):      
-          await message.client.send_message(message.to_id,"ты пидор")
+          await message.client.send_message(message.to_id,textt)
           asyncio.sleep(0,5)
